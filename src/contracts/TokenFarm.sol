0eb51d5ad99934d4279cc95fbff7d317a20949ad
deffiedefi <76966041+deffiedefi@users.noreply.github.com>
2021-02-22 11:13:27 -0800

    added testing for yieldfarm with truffletest and updated tokenfarm

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -10,8 +10,32 @@ contract TokenFarm {
     DappToken public dappToken;
     DaiToken public daiToken;
 
+    address[] public stakers;
+    mapping (address => uint) public stakingBalance;
+    mapping (address => bool) public hasStaked;
+    mapping (address => bool) public isStaking;
+
     constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
     }
+
+    //1. Stakes Tokens (Deposit)
+    function stakeTokens(uint _amount) public {
+         //Transfer Mock Dai to this contract for staking
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+         // update staking balance
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+         // add user to stakers array *only* if they haven't staked yet
+         if(!hasStaked[msg.sender]) {
+             stakers.push(msg.sender);
+         }
+        //update staking status
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
+    }
+    //2.Unstake Tokens (Withdraw)
+
+    //3.Issuing Tokens 
 }
--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -10,18 +10,61 @@ require('chai')
     .use(require('chai-as-promised'))
     .should()
 
-contract('TokenFarm', (accounts) => {
-    let daiToken
+function tokens (n) {
+    return web3.utils.toWei(n, 'ether');
+}
+
+contract('TokenFarm', ([owner, investor]) => {
+    let daiToken, dappToken, tokenFarm
 
     before(async () => {
+        // load contracts
         daiToken = await DaiToken.new()
+        dappToken = await DappToken.new()
+        tokenFarm = await TokenFarm.new(dappToken.address, daiToken.address)
+
+        // Transfer all DApp tokens to farm (1 million)
+
+        await dappToken.transfer(tokenFarm.address, tokens('1000000'))
+// Send tokens to investor
+        await daiToken.transfer(investor, tokens('100'), { from: owner })
     })
-// write tests here...
-    describe('Mock Dai deployment', async () => {
+
+    describe('Mock DAI deployment', async () => {
         it('has a name', async () => {
             const name = await daiToken.name()
             assert.equal(name, 'Mock DAI Token')
         })
     })
 
-})   
+    describe('Dapp Token deployment', async () => {
+        it('has a name', async () => {
+            const name = await dappToken.name()
+            assert.equal(name, 'Dapp Token')
+        })
+    })
+
+    describe('Token Farm deployment', async () => {
+        it('has a name', async () => {
+            const name = await tokenFarm.name()
+            assert.equal(name, 'TokenFarm')
+        })
+
+        it('contract has tokens', async () => {
+            let balance = await dappToken.balanceOf(tokenFarm.address)
+            assert.equal(balance.toString(), tokens('1000000'))
+        })
+    })
+
+    describe('Farming Tokens', async () => {
+
+        it('rewards investors for staking mDai tokens', async () => {
+            let result
+            // check investor balance for staking
+            result = await daiToken.balanceOf(investor)    
+            assert.equal(result.toString(), tokens('100'), 'investor MockDAI wallet balance correct before staking')
+        })
+    })
+
+
+})
