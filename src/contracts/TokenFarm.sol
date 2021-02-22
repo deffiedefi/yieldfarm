8de0b62402499d6428f75389caf6772978099698
deffiedefi <76966041+deffiedefi@users.noreply.github.com>
2021-02-21 21:07:41 -0800

    created testing suite with truffletest for tokenfarm

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,7 +1,17 @@
 pragma solidity^0.5.0;
+
+import "./DappToken.sol";
+import "./DaiToken.sol";
 // declare what version of solidity we're using
 contract TokenFarm {
     // All code goes here...
     // Declare string name
     string public name = "TokenFarm";
+    DappToken public dappToken;
+    DaiToken public daiToken;
+
+    constructor(DappToken _dappToken, DaiToken _daiToken) public {
+        dappToken = _dappToken;
+        daiToken = _daiToken;
+    }
 }
